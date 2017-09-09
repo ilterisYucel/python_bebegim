337be7cd6ca7e2b8b9f12506e3522fd47976dc0c
ilteris yucel <ilteris.yucel@hacettepe.edu.tr>
2017-09-09 06:57:42 +0300

    Delete soru6.py

--- harici_dosyalar/mayın_soruları/soru6.py
+++
@@ -1,9 +0,0 @@
-
-#1'den 5'e kadar olan sayıların karesini ve küpünü
-#içeren 2 boyutlu bir liste üreten aşağıdaki fonksiyonu
-#tamamlayınız.(bir satır 'sayı, karesi, küpü' şeklindedir.)
-iki_boyut_liste = []
-for i in range(1, 6):
-    satir = [i, i**2, i**3]
-    iki_boyut_liste.append(satir)
-print(iki_boyut_liste)
